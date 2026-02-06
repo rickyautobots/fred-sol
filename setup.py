739dd7a03f80feb6963d6e85eaa0a0685e0b2356
Derek Passmore <rickyautobots@gmail.com>
2026-02-06 06:48:29 -0600

    Add setup.py for editable installs

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+FRED-SOL Setup Script
+
+For editable installation:
+    pip install -e .
+
+For development:
+    pip install -e ".[dev]"
+
+For full installation with dashboard:
+    pip install -e ".[full]"
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
